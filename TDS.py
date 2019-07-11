13216c56f41d5cdb0a800c894fc8ba2a12ade69a
EC2 Default User <ec2-user@ip-172-31-37-109.us-east-2.compute.internal>
2019-07-11 17:52:35 +0000

    fenished

--- TDS.py
+++ TDS.py
@@ -1,11 +1,18 @@
 x = input("What variable do you not know? s- for speed. t- for time. d- for distance.")
 if "s" == x:
-    d = input("What is your distance? ")
-    t = input("What is your time? ")
-    print (str(d/t) + "is speed") 
+    d = float(input("What is your distance? "))
+    t = float(input("What is your time? "))
+    print (str(d/t) + "m/s is your speed") 
 elif "t" == x:
-    d = input("What is your distance? ")
-    s = input("What is your speed? ")
+    d = float(input("What is your distance? "))
+    s = float(input("What is your speed? "))
+    print (str(d/s) + "s is your time") 
+
 elif "d" == x:
-    s = input("What is your speed? ")
-    t = input("What is your time? ")
+    s = float(input("What is your speed? "))
+    t = float(input("What is your time? "))
+    print (str(s*t) + "m is your distance") 
+else:
+    print("Error input invalet")
+    
+#dts = 0
